d032a87dcacea76d5d4e2f1f8fc6d85799d0960b
shalini9511 <138895099+shalini9511@users.noreply.github.com>
2023-07-26 18:22:37 +0530

    Update index-with-require.js

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -3,7 +3,18 @@ const today = require('./today');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end(`Hello, World! The date today is ${today.getDate()}`);
+  let dateval = today.getDate();
+  let greeting = "It is still not morning"
+  if(dateval.getHours()>6 && dateval.getHours()<12){
+    greeting = "Good Morning!"
+  } else if (dateval.getHours()>=12 && dateval.getHours()<18){
+    greeting = "Good Afternoon!"
+  } else if (dateval.getHours()>=18 && dateval.getHours()<21){
+    greeting = "Good Evening!"   
+  } else if (dateval.getHours()>=21 && dateval.getHours()<24){
+    greeting = "Good Night!"
+  }
+  res.end(`Hello,${greeting}`);
 }
 
 const port = 8080;
